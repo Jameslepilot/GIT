3539a009fddf43e02ac640c4daebfd44b3066056
JamesLePilkot <jameslepilot@gmail.com>
2022-01-26 21:15:01 +0100

    dsad

--- ANTIGUO/test_prueba.py
+++ ANTIGUO/test_prueba.py
@@ -4,3 +4,9 @@ import estoesunaprueba as estoesunaprueba
 def test_pruebas():
     assert estoesunaprueba.suma(4, 1) == 5
     assert estoesunaprueba.resta(5, 1) == 4
+    assert estoesunaprueba.resta(-3, 1) == -4
+    assert estoesunaprueba.resta(-9, 1) == -10
+    assert estoesunaprueba.suma(5, 1) == 6
+    assert estoesunaprueba.suma(5, -2) == 3
+    assert estoesunaprueba.resta(5, -20) == 25
+
